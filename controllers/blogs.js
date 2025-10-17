c699052dce2c55ed3cf7582954dc9263cf920723
mauroperez055 <mauroperez055@gmail.com>
2025-10-17 20:56:07 -0300

    Actualizo con supertest

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,75 +1,60 @@
-const blogsRouter = require('express').Router();
-const Blog = require('../models/blog');
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
 
 //obtengo todos los blogs
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-      response.json(blogs)
-    })
-})
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
 //obtengo un blog por id
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
+});
 
 //creo un nuevo blog
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
-  })
+    likes: body.likes || 0,
+  });
 
-  blog
-    .save()
-    .then(savedBlog => {
-      response.json(savedBlog)
-    })
-    .catch(error => next(error))
-})
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
 
 //elimino un blog por id
-blogsRouter.delete("/:id", (request, response, next) => {
-  Blog.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch(error => next(error));
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 
 //actualizo un blog por id
-blogsRouter.put('/:id', (request, response, next) => {
-  const { title, author, url, likes } = request.body
+blogsRouter.put("/:id", async (request, response) => {
+  const { title, author, url, likes } = request.body;
 
-  Blog.findById(request.params.id)
-    .then((blog) => {
-      if (!blog) {
-        return response.status(404).end() // Si el blog no existe
-      }
+  const blog = await Blog.findById(request.params.id);
+  if (!blog) {
+    return response.status(404).end(); // Si el blog no existe
+  }
 
-      // Actualizamos los campos
-      blog.title = title
-      blog.author = author
-      blog.url = url
-      blog.likes = likes
+  // Actualizamos los campos
+  blog.title = title;
+  blog.author = author;
+  blog.url = url;
+  blog.likes = likes;
 
-      return blog.save().then((updatedBlog) => {
-        response.json(updatedBlog) // Respondemos con el blog actualizado
-      })
-    })
-    .catch((error) => next(error)) // Pasamos errores al middleware de manejo de errores
-})
+  const updatedBlog = await blog.save();
+  response.json(updatedBlog);
+});
 
 module.exports = blogsRouter;
---
+++ test/blog_api.test.js
@@ -0,0 +1,32 @@
+const { test, after, beforeEach } = require('node:test');
+const assert = require('node:assert');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const Blog = require('../models/blog');
+const mongoose = require('mongoose');
+
+// Datos iniciales de ejemplo
+const initialBlogs = [
+  { title: 'Primer blog', author: 'Mauro', url: 'http://ejemplo1.com', likes: 5 },
+  { title: 'Segundo blog', author: 'Perez', url: 'http://ejemplo2.com', likes: 10 },
+]
+
+// Limpia la base de datos antes de cada test
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('all blogs are returned as JSON', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(response.body.length, initialBlogs.length);
+})
+
+after(async () => {
+  await mongoose.connection.close();
+})
